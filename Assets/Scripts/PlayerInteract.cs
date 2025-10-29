a02736f99d4e40e3b3f061c5cbed3e73340400a7
Pedro Chaidez <149631039+Pedro-Chaidez@users.noreply.github.com>
2025-10-29 12:53:18 -0700

    Added UI text for interactions and got rid of temp ghost class

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -7,19 +7,29 @@ public class PlayerInteract : MonoBehaviour
     private float distance = 3f;
     [SerializeField]
     private LayerMask mask;
+    private PlayerUI playerUI;
+    private InputManager inputManager;
     private void Start()
     {
         cam = GetComponent<PlayerLook>().cam;
+        playerUI = GetComponent<PlayerUI>();
+        inputManager = GetComponent<InputManager>();
     }
     private void Update()
     {
+        playerUI.UpdateText(string.Empty);
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * distance);
         RaycastHit hitInfo;
         if(Physics.Raycast(ray, out hitInfo, distance, mask))
         {
-            if(hitInfo.collider.GetComponent<Interactable>() != null) {
-                Debug.Log(hitInfo.collider.GetComponent<Interactable>().promptMessage);
+            Interactable interactable = hitInfo.collider.GetComponent<Interactable>();
+            if(interactable != null) {
+                playerUI.UpdateText(interactable.promptMessage);
+                if(inputManager.onFoot.Interact.triggered)
+                {
+                    interactable.BaseInteract();
+                }
             }
         }
     }
