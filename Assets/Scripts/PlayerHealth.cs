26f8e24a4d89387e3d9fd9e64df853dfce5b3ad4
colintruong <colintruong168@gmail.com>
2025-11-06 09:30:47 -0800

    Death screen works
    Working on trying to get a functional resurrect button to work when the death screen pops up

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
     public float maxHealth = 100f;
     public float currentHealth;
+    public GameObject diedScreen;
 
     private void Start() {
         currentHealth = maxHealth;
         Debug.Log("Player Health: " + currentHealth);
+        if (diedScreen != null) {
+            diedScreen.SetActive(false);
+        }
     }
 
     public void TakeDamage(float amount) {
@@ -21,5 +27,20 @@ public class PlayerHealth : MonoBehaviour {
 
     private void Die() {
         Debug.Log("You died!");
+        if (diedScreen != null) {
+            diedScreen.SetActive(true);
+        }
+        GetComponent<InputManager>().enabled = false;
+    }
+
+    public void Ressurect() {
+        Debug.Log("Resurrected");
+        //SceneManager.LoadScene("Test");'
+        if (diedScreen != null) {
+            diedScreen.SetActive(false);
+            currentHealth = maxHealth;
+            GetComponent<InputManager>().enabled = false;
+        }
+        Debug.Log("Player Health: " + currentHealth);
     }
 }
