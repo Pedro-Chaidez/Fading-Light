40bdafca440ee15607a0422ee8a6e93336248e2a
colintruong <colintruong168@gmail.com>
2025-11-12 14:01:21 -0800

    Health bar added
    I added a dynamic health bar. It changes colors based on the amount of health the player has. I implemented a neat flashing mechanic when the player is under a certain threshold. I used this video for guidance: https://www.youtube.com/watch?v=ZzkIn41DFFo

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
     public float maxHealth = 100f;
     public float currentHealth;
+    public Image healthBar;
     public GameObject diedScreen;
+    public float lerp;
+    float lerpSpeed;
+
+    [Header("Low Health Flash Settings")]
+    public float lowHealthThreshold = 25f;
+    public float flashSpeed = 4f;
+    private bool isLowHealthFlashing = false;
 
     private PlayerCoordinates playerCoordinates;
     private CharacterController character;
@@ -19,6 +27,35 @@ public class PlayerHealth : MonoBehaviour {
         playerCoordinates = GetComponent<PlayerCoordinates>();
         character = GetComponent<CharacterController>();
     }
+    private void Update() {
+        if (currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        }
+
+        lerpSpeed = lerp * Time.deltaTime;
+
+        HealthBarFiller();
+        ColorChanger();
+    }
+
+    void HealthBarFiller() {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, currentHealth / maxHealth, lerpSpeed);
+    }
+
+    void ColorChanger() {
+        float healthPercent = currentHealth / maxHealth;
+
+        if (currentHealth <= lowHealthThreshold && currentHealth > 0) {
+            isLowHealthFlashing = true;
+            float t = Mathf.Abs(Mathf.Sin(Time.time * flashSpeed)); // oscillates 0–1
+            Color flashColor = Color.Lerp(new Color(0.5f, 0, 0, 0.8f), Color.red, t);
+            healthBar.color = flashColor;
+        } else {
+            isLowHealthFlashing = false;
+            Color healthColor = Color.Lerp(Color.red, Color.green, healthPercent);
+            healthBar.color = healthColor;
+        }
+    }
 
     public void TakeDamage(float amount) {
         currentHealth -= amount;
@@ -28,6 +65,12 @@ public class PlayerHealth : MonoBehaviour {
         if (currentHealth <= 0f) {
             Die();
         }
+    }
+
+    public void Heal(float amount) {
+        currentHealth += amount;
+        currentHealth = Mathf.Min(currentHealth, 100f);
+        Debug.Log("Player Health: " + currentHealth);
     }
 
     private void Die() {
