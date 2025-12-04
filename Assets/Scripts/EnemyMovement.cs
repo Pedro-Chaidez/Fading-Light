cfc10dc0080366cd51eb815c9d153aafda40006a
Pedro Chaidez <149631039+Pedro-Chaidez@users.noreply.github.com>
2025-12-03 16:52:54 -0800

    Added a game loop

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -31,7 +31,7 @@ public class EnemyMovement : MonoBehaviour
     public float facingThreshold = 0.9f;
     public bool stunned = false;
 
-    private void Start()
+    private void Awake()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
         playertransforms = new Transform[players.Length];
@@ -47,6 +47,15 @@ public class EnemyMovement : MonoBehaviour
 
     private void Update()
     {
+        if(players.Length == 0)
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            playertransforms = new Transform[players.Length];
+            for (int i = 0; i < players.Length; i++)
+            {
+                playertransforms.SetValue(players[i].transform, i);
+            }
+        }
         if (playerInView())
         {
             outOfSight = 0f;
